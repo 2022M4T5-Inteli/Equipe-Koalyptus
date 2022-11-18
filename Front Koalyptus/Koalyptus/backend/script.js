0b3d8d87a236112438c242eda1b87c2a1dd986ed
Lívia Bonotto <liviabonotto@gmail.com>
2022-11-18 11:09:13 -0300

    Upload: Front Sprint 3

---
+++ Front Koalyptus/Koalyptus/backend/script.js
@@ -0,0 +1,20 @@
+// Puxar dados de cada estufa
+// Fazer filtro de pesquisa por texto das estufas
+// Adicionar uma nova estufa/dispositivo (+)
+// Editar estufa/dispositivo (modal)
+// Apagar estufa/dispositivo (modal)
+// Chamar imagens de alerta para cada caso (triângulo vermelho ou amarelo)
+// Chamar mensagens de alerta para cada caso de alerta
+// Criar botão de enviar confirmação (integração com o teams)
+// Fazer sincronização de intervalo e faixas (altera em todas as estufas)
+// Código do histórico de temperatura e umidade (gráfico e tabela)
+// Dropdown gráfico de linha e tabela 
+// Filtro por data
+// Modal com dropshadow quando estiver selecionado
+// Modal com fundo cinza quando estiver inativo
+
+
+// GRÁFICO DE LINHA
+//https://www.chartjs.org/docs/latest/samples/line/line.html
+//https://www.chartjs.org/docs/latest/samples/animations/loop.html //LINHA CURVA
+// ANIMAÇÃO: https://www.chartjs.org/docs/latest/samples/animations/progressive-line.html
