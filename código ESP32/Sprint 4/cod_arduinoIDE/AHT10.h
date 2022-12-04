dd1482405695d859206c2d793de4f057a2f0db62
Luana Dinamarca Parra <99224178+luanaparra@users.noreply.github.com>
2022-12-04 17:54:08 -0300

    update código

--- código ESP32/Sprint 4/cod_arduinoIDE/AHT10.h
+++ código ESP32/Sprint 4/cod_arduinoIDE/AHT10.h
@@ -17,12 +17,14 @@ int sensorSetup(){
   return sensorWork;
 }
 
+// Criação de variável da leitura de temperatura
 float getTemp(){
   sensors_event_t humidity, temp; //Criação de variáveis para guardar a temperatura lida pelo sensor
   aht.getEvent(&humidity, &temp); //Inserção do valor de temperatura na variável criada
   return temp.temperature; //Retorno da temperatura
 }
 
+// Criação de variável da leitura de umidade
 float getHum(){
   sensors_event_t humidity, temp; //Criação de variáveis para guardar a umidade lida pelo sensor
   aht.getEvent(&humidity, &temp); //Inserção do valor de umidade na variável criada
